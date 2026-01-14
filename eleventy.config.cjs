b80645ddfc27d22cbd1bf958414284e6b5717135
thnkk <khxynt96bs@privaterelay.appleid.com>
2026-01-14 14:22:24 +0100

    Create eleventy.config.cjs

---
+++ eleventy.config.cjs
@@ -0,0 +1,36 @@
+const markdownIt = require("markdown-it");
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  const md = markdownIt({ html: true, breaks: true, linkify: true });
+  eleventyConfig.setLibrary("md", md);
+
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/robots.txt": "robots.txt" });
+
+  eleventyConfig.addFilter("isoDate", (dateObj) =>
+    DateTime.fromJSDate(dateObj, { zone: "utc" }).toISODate()
+  );
+  eleventyConfig.addFilter("readableDate", (dateObj) =>
+    DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd LLL yyyy")
+  );
+
+  eleventyConfig.addCollection("texts", (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob("src/texts/*.md")
+      .sort((a, b) => b.date - a.date);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["md", "njk", "html"],
+  };
+};
